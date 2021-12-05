b76a5f7c05ca7bbb6fd8dd7a81b46f21d15e27b4
Kaileigh Bonaccorsi <kaileigh.bonaccorsi@gmail.com>
2021-12-05 09:00:29 -0600

    added apollo server to server.js, and apollo-server-express to server package.json

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const path = require('path');
-const db = require('./config/connection');
 const routes = require('./routes');
+//import the ApolloServer class
+const { ApolloServer } = require('apollo-server-express');
 
-// 1. Set up an Apollo Server to use GraphQL queries and mutations to fetch and modify data, replacing the existing RESTful API.
+//import the two parts of a GraphQL schema
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
 
-//* `server.js`: Implement the Apollo Server and apply it to the Express server as middleware.
-
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
-app.use(express.urlencoded({ extended: true }));
+//create a new instance of an Apollo server with the GraphQL schema
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
+
+//Implement the Apollo Server and apply it to the Express server as middleware.
+server.applyMiddleware({ app });
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
@@ -21,5 +31,8 @@ if (process.env.NODE_ENV === 'production') {
 app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`)
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
